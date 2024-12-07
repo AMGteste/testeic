6119ad8d9600b031e81bba5dfe443d242119669b
AMGteste <tojotamendes@gmail.com>
2024-12-07 16:58:48 +0000

    Update main1_api.py

--- main1_api.py
+++ main1_api.py
@@ -1,7 +1,12 @@
-import os
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  5 19:59:37 2024
+
+@author: guerr
+"""
+
 import uvicorn
 from api import app  # Importa o app do FastAPI
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
